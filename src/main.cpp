0a42a0400ebf2483c4d4feeb27e2e8d0395a205f
Keberson <creedkiller.62@gmail.com>
2022-01-11 18:09:49 +0300

    Add code to create custom filename

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,15 @@
 
 #define LOGFILE_NAME "logfile.log"
 
-int main() {
+int main(int argc, char* argv[]) {
     srand(time(nullptr));
-    Model model(LOGFILE_NAME);
+
+    std::string filename = LOGFILE_NAME;
+    if (argc >= 2) {
+        filename = argv[1];
+    }
+
+    Model model(filename);
     model.startSimulation();
 
     return 0;
